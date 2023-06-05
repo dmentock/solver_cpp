86c3e68cb91a45e107daf6fce1507fd241167d9d
Daniel Otto de Mentock <d.mentock@mpie.de>
2023-06-05 18:08:00 +0200

    no longer needed

--- include/test/array_matcher.h
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-
-// make array pointer comparable for testing by converting it into a tuple
-MATCHER_P2(ArrayPointee, size, subMatcher, "")
-{
-    return ExplainMatchResult(subMatcher, std::make_tuple(arg, size), result_listener);
-}
